2d7a19d12cfa786e7d7d8175ed0714098b6a8197
Dhruv Engineer <72173316+Dengineer24@users.noreply.github.com>
2022-05-21 16:28:39 -0400

    Update Help.java

--- src/main/java/com/dengineer/commands/Help.java
+++ src/main/java/com/dengineer/commands/Help.java
@@ -4,9 +4,14 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.*;
 
 import java.awt.Color;
 import java.lang.StringBuilder;
+
+import javax.swing.plaf.ColorUIResource;
 
 public class Help extends ListenerAdapter {
 
@@ -14,19 +19,15 @@ public class Help extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         EmbedBuilder eb = new EmbedBuilder();
         StringBuilder sb = new StringBuilder();
-        eb.setColor(Color.MAGENTA);
+        eb.setColor(new ColorUIResource(90, 70, 155));
         eb.setTitle("Help is here!");
         eb.setDescription("Full Command Center and Guide for DengiBot!");
         eb.setAuthor("Dengibot", "https://dengineer.vercel.app", "https://dengineer.vercel.app/favicon.png");
         eb.setThumbnail("https://dengineer.vercel.app/favicon.png");
-        sb = sb.append(">!help:\n\n  Brings up this menu. Used to see all commands.\n\n");
-        sb = sb.append(">!todo:\n\n  Brings up to-do list, for the user.\n\n");
-        sb = sb.append(">!shoe:\n\n Stay on top of new sneaker relesases.\n\n");
         eb.addField("Commands:", sb.toString(), false);
-        eb.addField("Help", "Brings up this menu. Used to see all commands", true).addBlankField(true);
-        eb.setFooter("Visit: [links](Https://dengineer.vercel.app)");
-        
-
+        eb.addField("!Help", "`Brings up this menu. Used to see all commands`", true).addBlankField(true);
+        eb.addField("!todo", "`Gives today's to-do list`", false);
+        eb.addField("!shoe", "`Gives shoe drop information for the coming days`", false);
         MessageChannel channel = event.getChannel();
         channel.sendMessageEmbeds(eb.build()).queue();
     }
